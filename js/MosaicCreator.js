4fa43e4e650cbcb82329809a66acac7a19739e11
Hernan Garchtrom <hernang87@gmail.com>
2017-01-21 21:13:13 -0300

    Finished

--- js/MosaicCreator.js
+++ js/MosaicCreator.js
@@ -2,10 +2,6 @@ class MosaicCreator {
     constructor(options = {}) {
         this.tileHeight = options.tileHeight || TILE_HEIGHT;
         this.tileWidth = options.tileWidth || TILE_WIDTH;
-
-        this.maxWidth = options.maxWidth || 600;
-        this.maxHeight = options.maxHeight || 600;
-        this.detail = options.detail || 5;
         this.pixelInterval = options.pixelInterval || 5;
     }
 
@@ -16,21 +12,25 @@ class MosaicCreator {
     }
 
     createMosaic(data) {     
+        // Got imageData, creating image object to get size
         const image = new Image();
         image.src = data;
 
+        // Helper canvas, will not be rendered
         const canvas = document.createElement('canvas');
         const context = canvas.getContext('2d');
 
         canvas.width = image.width;
         canvas.height = image.height;
         
+        // Mosaic canvas, where the final mosaic will be rendered
         const mosaic = document.createElement('canvas');
         const mosaicCtx = mosaic.getContext('2d');
         mosaic.width = image.width;
         mosaic.height = image.height;
         mosaic.className = 'result-canvas';
 
+        // Get how many rows and columns the image has
         const columns = Math.ceil(image.width / this.tileWidth);
         const rows = Math.ceil(image.height / this.tileHeight);           
 
@@ -40,18 +40,25 @@ class MosaicCreator {
                 let deltaX = x * this.tileWidth;
                 let deltaY = y * this.tileHeight;              
 
+                // Draw the tile on the helper canvas to get it's binary data
                 context.drawImage(image, deltaX, deltaY, this.tileWidth, this.tileHeight, 0, 0, this.tileWidth, this.tileHeight);   
 
+                // Get binary data of current tile
                 let tile = context.getImageData(0, 0, this.tileWidth, this.tileHeight);                                                               
 
                 let rgb = this.getTileColor(tile);
                 
+                // We want to render one row at a time, so we need an array 
+                // of the tiles to be rendered there.
+                // I use RGB for simplicity here, but could use HEX.            
                 rowTiles.push({
                     fillStyle: `rgb(${rgb.r},${rgb.g},${rgb.b}`,
-                    deltaX, deltaY
+                    deltaX, 
+                    deltaY
                 });                            
             }
 
+            // Render each tile of the current row
             for(let tile of rowTiles) {
                 mosaicCtx.fillStyle = tile.fillStyle;
                 mosaicCtx.fillRect(tile.deltaX, tile.deltaY, this.tileWidth, this.tileHeight);
@@ -67,14 +74,21 @@ class MosaicCreator {
         
         let data = tile.data;              
         let totalPixels = 0;
+
+        // To start at the beginning
         let currentPixel = -this.pixelInterval + 1;
-        while ((currentPixel += this.pixelInterval * 4) < data.length) {              
+
+        // This loop moves by the pixelInterval variable
+        // The smaller the pixelInterval is the higher the confidende of the average color is
+        while ((currentPixel += this.pixelInterval * (this.pixelInterval-1)) < data.length) {              
               rgb.r += data[currentPixel];
               rgb.g += data[currentPixel + 1];
               rgb.b += data[currentPixel + 2];
               totalPixels++;
           }
           
+          // To get the average of the analyzed pixels
+          // Rounding down so we get an int
           rgb.r = Math.floor(rgb.r / totalPixels);
           rgb.g = Math.floor(rgb.g / totalPixels);
           rgb.b = Math.floor(rgb.b / totalPixels);          
